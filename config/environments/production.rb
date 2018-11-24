861ac86d8ef2b9c4f5bf145f98fbf0d952af7894
Lali Gabelaya <laligabelaya@gmail.com>
2018-11-23 21:26:38 -0800

    config production fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "dfmfit_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'https://crossfit2120.herokuapp.com' }
 
 
 
