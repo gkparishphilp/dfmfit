4e16eeeccd402eb5e21ea7982a936657027fc11f
Lali Gabelaya <laligabelaya@gmail.com>
2019-01-19 04:53:01 -0800

    mailer

--- app/mailers/waiver_mailer.rb
+++ app/mailers/waiver_mailer.rb
@@ -1,9 +1,10 @@
 class WaiverMailer < ActionMailer::Base
   default from: "https://crossfit2120.herokuapp.com/"
-  default to: "lali20021@gmail.com"
+
 
   def waiver_notification(waiver)
     @waiver = waiver
-   mail(subject: 'New signed waiver at Crossfit2120')
+   mail(to: "lali20021@gmail.com",
+        subject: 'New signed waiver at Crossfit2120')
  end
 end
