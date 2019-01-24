fd902217c3c0c826fedc77789c1c82006090487c
Lali Gabelaya <laligabelaya@gmail.com>
2019-01-24 13:59:24 -0800

    modal

--- app/assets/javascripts/custom/promo_popper.js
+++ app/assets/javascripts/custom/promo_popper.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
-  let modal_triggered = false;
+  var modal_triggered = false;
   return $( window ).scroll(function() {
     const height = $(window).scrollTop();
     if ((height > 500) && !( modal_triggered )) {
