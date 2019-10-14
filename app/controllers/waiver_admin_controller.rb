23ecc4f728ba17f8c97705a0d402d5e1cf33a45b
Lali Gabelaya <laligabelaya@gmail.com>
2019-10-14 14:07:22 -0700

    updated schedule

--- app/controllers/waiver_admin_controller.rb
+++ app/controllers/waiver_admin_controller.rb
@@ -2,11 +2,10 @@ class WaiverAdminController < ApplicationAdminController
 
     def index
       @waivers = Waiver.page( params[:page] )
-
     end
 
     def show
       @waiver = Waiver.find(params[:id])
     end
 
-  end
+end
